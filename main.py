5c7a731300d5294638004faaa778879cb8957940
Tomáš Turecký <t_turecky@utb.cz>
2024-11-28 10:38:44 +0100

    Ngrams functions combined into one to avoid needless loops.

--- main.py
+++ main.py
@@ -8,13 +8,9 @@ def main():
     en_corpus_string = corpus_to_string("en_corpus.txt", en_chars)
     print(en_corpus_string)
 
-    en_unigrams = get_unigrams(en_corpus_string)
+    en_unigrams, en_bigrams, en_trigrams = get_ngrams(en_corpus_string)
     print(en_unigrams)
-
-    en_bigrams = get_bigrams(en_corpus_string)
     print(en_bigrams)
-
-    en_trigrams = get_trigrans(en_corpus_string)
     print(en_trigrams)
 
 
@@ -36,25 +32,11 @@ def corpus_to_string(corpus, chars):
     return corpus_string
 
 
-def get_unigrams(corpus_string):
+
+
+def get_ngrams(corpus_string):
     unigrams = {}
-
-    for char in corpus_string:
-        unigrams[char] = unigrams.get(char, 0) + 1
-
-    return unigrams
-
-def get_bigrams(corpus_string):
     bigrams = {}
-
-    for x,char in enumerate(corpus_string):
-        if x < len(corpus_string)-1:
-            next_char = corpus_string[x+1]
-            bigrams[char+next_char] = bigrams.get(char+next_char, 0) + 1
-
-    return bigrams
-
-def get_trigrans(corpus_string):
     trigrams = {}
 
     for x,char in enumerate(corpus_string):
@@ -62,10 +44,12 @@ def get_trigrans(corpus_string):
             next_char1 = corpus_string[x+1]
             next_char2 = corpus_string[x+2]
             trigrams[char+next_char1+next_char2] = trigrams.get(char+next_char1+next_char2, 0) + 1
+        if x < len(corpus_string)-1:
+            next_char = corpus_string[x+1]
+            bigrams[char+next_char] = bigrams.get(char+next_char, 0) + 1
+        unigrams[char] = unigrams.get(char, 0) + 1
 
-    return trigrams
-
-
+    return unigrams, bigrams, trigrams
 
 
 if __name__ == '__main__':
